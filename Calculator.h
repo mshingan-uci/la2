408e8e49e12921d07a50c4a1419c8d82f473d220
Vincent Tran <neovintran@gmail.com>
2020-03-28 01:24:21 -0700

    Starter code commit

---
+++ Calculator.h
@@ -0,0 +1,34 @@
+// Add include guards 
+
+
+class Calculator {
+
+	double value;
+	char prevop; 
+
+public:
+
+	// Constructor; initializes value to 0.0 and
+	// pvop to '+'
+	Calculator();
+
+	// Performs calculation and  stores as value
+	void calculate(double x, double y, char op);
+
+	// Takes value, performs previous operation on
+	// itself
+	void repeat();
+
+	// Outputs value to stdout on its own line
+	void display();
+	
+	// Destructor; does nothing
+	~Calculator();
+
+private:
+
+	// Helper function; performs calculation and
+	// returns value
+	double calc(double x, double y, char op);
+
+};
